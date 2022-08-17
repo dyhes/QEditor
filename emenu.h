da4e8ca514c5e7f0b2496434074b92b3fba2d77a
A1120202073 <ee4b641a84884773a9611be9a01ba78c@example.com>
2022-08-17 19:26:42 +0800

    submit

---
+++ emenu.h
@@ -0,0 +1,60 @@
+#ifndef EMENU_H
+#define EMENU_H
+
+#include <QWidget>
+#include <QMenu>
+#include <QMenuBar>
+#include <QAction>
+#include "editor.h"
+
+class EMenu : public QMenuBar
+{
+    Q_OBJECT
+private:
+    bool is_open ;
+    bool is_new_file ;
+    QString file_name_current;
+    QString file_string_current;
+public:
+    explicit EMenu(QWidget *parent = nullptr);
+    QMenu *fileMenu;
+    QMenu *editMenu;
+    QMenu *runMenu;
+    QMenu *helpMenu;
+
+    QAction *openAct;
+    QAction *saveAct;
+    QAction *saveAsAct;
+    QAction *newFileAct;
+
+    QAction *undoAct;
+    QAction *redoAct;
+    QAction *copyAct;
+    QAction *pasteAct;
+    QAction *cutAct;
+
+    QAction *compileAct;
+    QAction *runAct;
+    Editor *editor;
+    bool openTextByStream(const QString &aFileName);
+    bool saveTextByStream(const QString &aFileName);
+signals:
+
+public slots:
+
+    void on_open_click();
+    void on_save_click();
+    void on_new_file_click();
+    void on_save_as_click();
+
+    void on_copy_click();
+    void on_paste_click();
+    void on_cut_click();
+    void on_undo_click();
+    void on_redo_click();
+
+    void on_compile_click();
+    void on_run_click();
+};
+
+#endif // EMENU_H
